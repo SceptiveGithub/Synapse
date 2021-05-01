dd3565a878988443a0ea71d18e317f4984d58c65
Sceptive <63530231+SceptiveGithub@users.noreply.github.com>
2021-04-30 21:37:19 -0500

    Update Module.lua

--- Viper/Module.lua
+++ Viper/Module.lua
@@ -3,6 +3,7 @@ local RunService = game:GetService("RunService")
 _G.Viper = {}
 
 local Id = 1
+local ConsoleHistory = {}
 
 local function Format(Bytes)
     local Symbol = "B"
@@ -18,8 +19,64 @@ local function Format(Bytes)
 	return Bytes .. " " .. Symbol
 end
 
+local function Download(PackageName,File)
+    rconsoleprint("Collecting " .. PackageName)
+    table.insert(ConsoleHistory,"Collecting " .. PackageName)
+    
+    local Package = game:HttpGet("https://raw.githubusercontent.com/SceptiveGithub/Synapse/master/Viper/" .. File,true)
+    local Downloaded = 0
+    
+    rconsoleprint("\n   Downloading " .. File .. " (" .. Format(#Package) .. ")")
+    table.insert(ConsoleHistory,"\n   Downloading " .. File .. " (" .. Format(#Package) .. ")")
+    rconsoleprint("\n       |                                 | 0 B 0 B/s\n")
+    
+    if not isfolder("Viper") then
+        makefolder("Viper")
+    end
+    
+    writefile("Viper/" .. File,"")
+    
+    local Speed
+    local LastFrame = os.clock()
+    while Downloaded < #Package do
+        RunService.RenderStepped:Wait()
+        
+        Speed = (math.clamp(math.noise(os.clock()/100),-1,1) + 1) / 2 * 5000 + 3000  
+        Downloaded += Speed
+
+        if Downloaded > #Package then
+            Downloaded = #Package
+        end
+        
+        appendfile("Viper/" .. File,string.sub(Package,Downloaded - Speed + 1, Downloaded))
+        
+        local BarLength = math.floor((Downloaded/#Package) * 32)
+        local Bar = string.rep("#",BarLength)
+        local Space = string.rep(" ",32 - BarLength)
+        
+        rconsoleclear()
+        for i,Line in ipairs(ConsoleHistory) do
+            rconsoleprint(Line)
+        end
+        rconsoleprint("\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed / (os.clock() - LastFrame)) .. "/s eta " .. math.abs(math.round((#Package - Downloaded) / (Speed / (os.clock() - LastFrame)))) .. "s\n")
+        
+        LastFrame = os.clock()
+    end
+    
+    local BarLength = math.floor((Downloaded/#Package) * 32)
+    local Bar = string.rep("#",BarLength)
+    local Space = string.rep(" ",32 - BarLength)
+    
+    rconsoleclear()
+    for i,Line in ipairs(ConsoleHistory) do
+        rconsoleprint(Line)
+    end
+    rconsoleprint("\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed / (os.clock() - LastFrame)) .. "/s\nSuccessfully installed " .. PackageName .. "\n")
+    table.insert(ConsoleHistory,"\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed / (os.clock() - LastFrame)) .. "/s\nSuccessfully installed " .. PackageName .. "\n")
+end
+
 function _G.Viper.open()
-    local ConsoleHistory = {}
+    ConsoleHistory = {}
     
     rconsoleclear()
     rconsolename("Viper")
@@ -35,60 +92,11 @@ function _G.Viper.open()
         if Commands[1] == "vip" then
             if Commands[2] == "install" then
                 if Commands[3] == "viperbackend" then
-                    rconsoleprint("Collecting viperbackend")
-                    table.insert(ConsoleHistory,"Collecting viperbackend")
-                    
-                    local Package = game:HttpGet("https://raw.githubusercontent.com/SceptiveGithub/Synapse/master/Viper/viperbackend.txt",true)
-                	local Downloaded = 0
-                	
-                	rconsoleprint("\n   Downloading viperbackend.txt (" .. Format(#Package) .. ")")
-                	table.insert(ConsoleHistory,"\n   Downloading viperbackend.txt (" .. Format(#Package) .. ")")
-                	rconsoleprint("\n       |                                 | 0 B 0 B/s\n")
-                	
-                	if not isfolder("Viper") then
-                	    makefolder("Viper")
-                	end
-                	
-                	writefile("Viper/viperbackend.txt","")
-                	
-                	local Speed
-                	local LastFrame = os.clock()
-                	while Downloaded < #Package do
-                	    RunService.RenderStepped:Wait()
-                	    
-                	    Speed = (math.clamp(math.noise(os.clock()/100),-1,1) + 1) / 2 * 5000 + 3000
-                	    
-                	    if Downloaded + Speed > #Package then
-                	        Speed = #Package - Downloaded
-                	    end
-                	    
-                	    Downloaded += Speed
-                	    
-                	    appendfile("Viper/viperbackend.txt",string.sub(Package,Downloaded - Speed + 1, Downloaded))
-                	    
-                	    local BarLength = math.floor((Downloaded/#Package) * 32)
-                	    local Bar = string.rep("#",BarLength)
-                	    local Space = string.rep(" ",32 - BarLength)
-                	    
-                	    rconsoleclear()
-                	    for i,Line in ipairs(ConsoleHistory) do
-                	        rconsoleprint(Line)
-                	    end
-                	    rconsoleprint("\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed) .. "/s eta " .. math.abs(math.round((#Package - Downloaded) / (Speed / (os.clock() - LastFrame)))) .. "s\n")
-                	    
-                	    LastFrame = os.clock()
-                	end
-                	
-                    local BarLength = math.floor((Downloaded/#Package) * 32)
-            	    local Bar = string.rep("#",BarLength)
-            	    local Space = string.rep(" ",32 - BarLength)
-                	
-                	rconsoleclear()
-                	for i,Line in ipairs(ConsoleHistory) do
-            	        rconsoleprint(Line)
-            	    end
-                	rconsoleprint("\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed) .. "/s\nSuccessfully installed viperbackend\n")
-                	table.insert(ConsoleHistory,"\n       |" .. Bar .. "" .. Space .."| ".. Format(Downloaded) .. " " .. Format(Speed) .. "/s\nSuccessfully installed viperbackend\n")
+                    Download("viperbackend","viperbackend.txt")
+                elseif Commands[3] == "connection" then
+                    Download("connection","connection.json")
+                elseif Commands[3] == "config" then
+                    Download("config","config.json")
                 else
                     rconsoleprint("Package " .. Commands[3] .. " not recognized\n")
                     table.insert(ConsoleHistory,"Package " .. Commands[3] .. " not recognized\n")
@@ -143,4 +151,20 @@ function _G.Viper.execute(Code,Debug)
     else
         error("Code must be a string")
     end
+end
+
+function _G.Viper.close()
+    if not isfolder("Viper") then
+        makefolder("Viper")
+    end
+    
+    Connections = readfile("Viper/connection.json")
+    Parsed = HttpService:JSONDecode(Connections)
+    
+    local RequestId = Id
+    Id += 1
+    
+    table.insert(Parsed.Viper,false)
+    
+    writefile("Viper/connection.json",HttpService:JSONEncode(Parsed))
 end
